349dd9e201102bf41757310513fe325275bb0970
tomohiro0225 <9050h.6007f6006f@gmail.com>
2023-09-25 12:56:54 +0900

    【今日やったこと】 ①GplanningのTOPページコーディング

---
+++ gp-theme2/js/common.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.top-work__slider').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      dots: false,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+  });
+
+});
